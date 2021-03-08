e37f32ae361fde4aaaf650fa9402bf81c0ee33f1
0dzsy5 <0dzsy5@gmail.com>
2021-03-08 07:59:47 +0100

    move parm to top

--- D365FFo/DevT1/InitT1VM.ps1
+++ D365FFo/DevT1/InitT1VM.ps1
@@ -1,10 +1,12 @@
+parm (
+    $ServerRole,$AdminPassword
+)
 <# Preparation:
     #  Windows updated
     #  Antimalware scan
     #
     # Execute this script:
     # Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/dz0sy5/work-less/master/D365FFo/DevT1/InitT1VM.ps1'))
-    #$ErrorActionPreference="Stop";If(-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent() ).IsInRole( [Security.Principal.WindowsBuiltInRole] "Administrator")){ throw "Run command in an administrator PowerShell prompt"};If($PSVersionTable.PSVersion -lt (New-Object System.Version("3.0"))){ throw "The minimum version of Windows PowerShell that is required by the script (3.0) does not match the currently running version of Windows PowerShell." };$DefaultProxy=[System.Net.WebRequest]::DefaultWebProxy;$securityProtocol=@();$securityProtocol+=[Net.ServicePointManager]::SecurityProtocol;$securityProtocol+=[Net.SecurityProtocolType]::Tls12;[Net.ServicePointManager]::SecurityProtocol=$securityProtocol;Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/dz0sy5/work-less/master/D365FFo/DevT1/InitT1VM.ps1'))
 
     #$ErrorActionPreference="Stop";If(-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]"Administrator")){throw "Run command in an administrator PowerShell prompt"};If($PSVersionTable.PSVersion -lt (New-Object System.Version("3.0"))){ throw "The minimum version of Windows PowerShell that is required by the script (3.0) does not match the currently running version of Windows PowerShell." };$DefaultProxy=[System.Net.WebRequest]::DefaultWebProxy;$securityProtocol=@();$securityProtocol+=[Net.ServicePointManager]::SecurityProtocol;$securityProtocol+=[Net.SecurityProtocolType]::Tls12;[Net.ServicePointManager]::SecurityProtocol=$securityProtocol;Register-PSRepository -Default -ErrorAction SilentlyContinue; Set-PSRepository -Name "PSGallery" -InstallationPolicy Trusted;Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://raw.githubusercontent.com/dz0sy5/work-less/DEV/D365FFo/DevT1/InitT1VM.ps1'))
     #ToDo:
@@ -51,9 +53,7 @@
     # "BackupPath"="C:\\DynamicsBackup"
 #>
  
-parm (
-    $ServerRole,$AdminPassword
-)
+
 #region Vars
 $Owner = 'dz0sy5';
 $Repository = 'work-less';
